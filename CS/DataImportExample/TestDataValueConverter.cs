853fe2e566f8d154f14c23c19b6a2dc88d69911e
Anthony Sam <anton.sameishchev@devexpress.com>
2025-06-20 13:35:04 +0200

    Update TestDataValueConverter.cs

--- CS/DataImportExample/TestDataValueConverter.cs
+++ CS/DataImportExample/TestDataValueConverter.cs
@@ -1,16 +1,17 @@
-﻿using DevExpress.Drawing;
+using DevExpress.Drawing;
+using DevExpress.Spreadsheet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using DevExpres.Spreadsheet;
 
 namespace DataImportExample
 {
     #region #converter
-    class TestDataValueConverter : DevExpress.Spreadsheet.IDataValueConverter
+    class TestDataValueConverter : IDataValueConverter
     {
-        private bool IDataValueConverter_TryConvert(object value, int index, ref CellValue result) {
+
+        public bool TryConvert(object value, int index, out CellValue result) {
                 if (value is string) {
                     string strValue = value as string;
         
@@ -22,7 +23,7 @@ namespace DataImportExample
                     {
                         int str2int = 0;
         
-                        if (Int32.TryParse(strValue, ref str2int))
+                        if (Int32.TryParse(strValue, out str2int))
                             result = str2int;
                         else
                             result = strValue;
